6cbf8799fe20dd1956fca5930e25c418022d915b
Pedro Henrique <p13dr0h@gmail.com>
2025-05-19 02:07:21 -0300

    test: add tests

---
+++ infrastructure/src/test/java/category/CategoryMySQLGatewayTest.java
@@ -0,0 +1,70 @@
+package category;
+
+import br.com.ph3nr1qu3.core.category.Category;
+import br.com.ph3nr1qu3.infrastructure.category.persistence.CategoryJpaEntity;
+import br.com.ph3nr1qu3.infrastructure.category.persistence.CategoryMySQLGateway;
+import br.com.ph3nr1qu3.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MySQLGatewayTest
+public class CategoryMySQLGatewayTest {
+
+    @Autowired
+    private CategoryMySQLGateway gateway;
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Test
+    void givenAValidCategoryWhenCallsCreateShouldReturnANewCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A Categoria";
+        final var expectedActive = true;
+
+        final var cat = Category.newCategory(expectedName, expectedDescription, expectedActive);
+
+        Assertions.assertEquals(0, repository.count());
+
+        final var actualCat = gateway.create(cat);
+
+        Assertions.assertEquals(1, repository.count());
+
+        Assertions.assertEquals(cat.getId(), actualCat.getId());
+        Assertions.assertEquals(expectedDescription, actualCat.getDescription());
+        Assertions.assertEquals(expectedName, actualCat.getName());
+        Assertions.assertEquals(expectedActive, actualCat.isActive());
+        Assertions.assertEquals(cat.getCreatedAt(), actualCat.getCreatedAt());
+
+        final var entity = repository.findById(actualCat.getId().getValue()).get();
+        Assertions.assertNotNull(entity);
+    }
+
+
+    @Test
+    void givenAValidCategory_whenCallsUpdate_shoudReturnCategoryUpdated() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A Categoria";
+        final var expectedActive = true;
+
+        final var cat = Category.newCategory("expectedName", expectedDescription, expectedActive);
+        Assertions.assertEquals(0, repository.count());
+
+        repository.saveAndFlush( CategoryJpaEntity.from(cat));
+
+        final var actualCat = gateway.create(cat);
+
+        Assertions.assertEquals(1, repository.count());
+
+        Assertions.assertEquals(cat.getId(), actualCat.getId());
+        Assertions.assertEquals(expectedDescription, actualCat.getDescription());
+        Assertions.assertEquals(expectedName, actualCat.getName());
+        Assertions.assertEquals(expectedActive, actualCat.isActive());
+        Assertions.assertEquals(cat.getCreatedAt(), actualCat.getCreatedAt());
+
+        final var entity = repository.findById(actualCat.getId().getValue()).get();
+        Assertions.assertNotNull(entity);
+    }
+
+}
---
+++ infrastructure/src/test/java/category/persistence/CategoryRepositoryTest.java
@@ -0,0 +1,33 @@
+package category.persistence;
+
+import br.com.ph3nr1qu3.core.category.Category;
+import br.com.ph3nr1qu3.infrastructure.category.persistence.CategoryJpaEntity;
+import br.com.ph3nr1qu3.infrastructure.category.persistence.CategoryRepository;
+import category.MySQLGatewayTest;
+import org.hibernate.PropertyValueException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+
+@MySQLGatewayTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Test
+    void givenAnInvalidNullName_whenCallSave_shouldReturnError() {
+        final var aCat = Category.newCategory("Films", "Watched the most", true);
+        final var aEnt = CategoryJpaEntity.from(aCat);
+        aEnt.setName(null);
+
+        final var actExc = Assertions.assertThrows(DataIntegrityViolationException.class, () -> repository.save(aEnt));
+
+        final var actCau = Assertions.assertInstanceOf(PropertyValueException.class, actExc.getClass());
+
+        Assertions.assertEquals("name", actCau.getPropertyName());
+        Assertions.assertEquals("not-null property", actCau.getMessage());
+    }
+
+}
