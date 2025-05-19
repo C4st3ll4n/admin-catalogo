baee50fed4dc96855176e08267deb2c484a103dd
Pedro Henrique <p13dr0h@gmail.com>
2025-05-19 02:07:41 -0300

    chore: add migration that adds category entity

--- infrastructure/src/main/resources/db/migration/V1__initial.sql
+++ infrastructure/src/main/resources/db/migration/V1__initial.sql
@@ -1,3 +1,9 @@
 CREATE TABLE category (
- id integer primary key
+ id VARCHAR(36) NOT NULL primary key,
+ name VARCHAR(255) NOT NULL,
+ description VARCHAR(4000),
+ active BOOLEAN NOT NULL,
+ created_at DATETIME(6) NOT NULL,
+ updated_at DATETIME(6),
+ deleted_at DATETIME(6)
 );
